c7bb2764cf937d59cd145c05014f12d8e7487ce3
ganados <przemek4121@gmail.com>
2020-10-13 21:10:10 +0200

    FInal version?

---
+++ JiPPLab/item1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+void insertSort(int *&numArray, int argc){
+    int j;
+    int storedValue;
+ 
+    for(int i = 1; i < argc - 1; i++){
+        storedValue = numArray[i]; //safekeeping of value for exchange
+        j = i - 1; //
+ 
+        while(j >= 0 && numArray[j] > storedValue){
+            numArray[j + 1] = numArray[j]; //sorting
+            j -= 1;
+        }
+        numArray[j + 1] = storedValue; //swap values
+    }
+}
+
+int main(int argc, char* argv[]){
+
+    int *numArray = new int [argc];
+    for(int i = 0; i < argc - 1; i++){
+        numArray[i] = atoi(argv[i + 1]);
+//        std::cout << argv[i + 1] << " ";
+    }
+//    std::cout << std::endl;
+//    std::cout << std::endl << argc << std::endl;
+    insertSort(numArray, argc);
+
+    std::cout << "out = ";
+    for(int i = 0; i < argc - 1; i++){
+        std::cout << numArray[i] << " ";
+    }
+
+    return 0;
+}
