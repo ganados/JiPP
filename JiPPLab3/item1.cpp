b6c0c672f458e6b42955226c63ebf943092e04b1
ganados <przemek4121@gmail.com>
2020-11-01 22:39:34 +0100

    ...

--- JiPPLab3/item1.cpp
+++ JiPPLab3/item1.cpp
@@ -1,17 +1,60 @@
 #include <iostream>
+#include <vector>
 
 class matrixTask {
     private:
-        double** matrix = nullptr;
+        std::vector < std::vector < double > > matrix; 
     public:
-        matrixTask(int cols, int rows);
+        matrixTask(int rows, int cols);
+        matrixTask(int size);
         ~matrixTask();
-        void printMatrix();
+        void set(int posN, int posM, int value);
+        double get(int posN, int posM);
+        double add(matrixTask m2);
+        double subtract(matrixTask m2);
+        double multiply(matrixTask m2);
+        int cols();
+        int rows();
+        void print(matrixTask matrix);
+        void store(std::string fileName, std::string filePath);
+        matrixTask(std::string filePath);
 };
 
-matrixTask::matrixTask(int cols, int rows){
-    matrix = new double *[rows];
-    for(int i = 0; i < cols; i++)
-        matrix[i] = new double;
+matrixTask::matrixTask(int rows, int cols){
+    matrix.reserve(rows);
+    for(int i = 0; i < rows; i++)
+        matrix[i].reserve(cols);
 }
+matrixTask::matrixTask(int size){
+    matrix.reserve(size);
+    for(int i = 0; i < size; i++)
+        matrix[i].reserve(size);
+}
+
 matrixTask::~matrixTask(){};
+
+void matrixTask::set(int posN, int posM, int value){
+
+}
+
+double matrixTask::get(int posN, int posM){
+    return 1;
+}
+
+int matrixTask::cols(){
+    return matrix[0].size();
+}
+
+int matrixTask::rows(){
+    return matrix.size();
+}
+
+int main(){
+    matrixTask firstMatrix = matrixTask(3,4);
+    int size = firstMatrix.cols();
+    std::cout << size;
+    int maxSize = firstMatrix.rows();
+    std::cout << std::endl <<maxSize;
+
+    return 0;
+}
