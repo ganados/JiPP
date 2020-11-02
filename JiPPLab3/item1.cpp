7f0261c185da100b3b6fb6fd38779dfdeb9dd574
ganados <przemek4121@gmail.com>
2020-11-02 12:32:13 +0100

    IT WORKS

--- JiPPLab3/item1.cpp
+++ JiPPLab3/item1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
 class matrixTask {
     private:
@@ -8,33 +9,41 @@ class matrixTask {
         matrixTask(int rows, int cols);
         matrixTask(int size);
         ~matrixTask();
-        void set(int posN, int posM, int value);
+        void set(int posN, int posM, double value);
         double get(int posN, int posM);
         double add(matrixTask m2);
         double subtract(matrixTask m2);
         double multiply(matrixTask m2);
         int cols();
         int rows();
-        void print(matrixTask matrix);
+        void print();
         void store(std::string fileName, std::string filePath);
         matrixTask(std::string filePath);
 };
 
 matrixTask::matrixTask(int rows, int cols){
-    matrix.reserve(rows);
-    for(int i = 0; i < rows; i++)
-        matrix[i].reserve(cols);
+    std::vector <double> temp;
+    for(int j = 0; j < cols; j++){
+            temp.push_back(0);
+        }
+    for(int i = 0; i < rows; i++){
+        matrix.push_back(temp);
+    }
 }
 matrixTask::matrixTask(int size){
-    matrix.reserve(size);
-    for(int i = 0; i < size; i++)
-        matrix[i].reserve(size);
+    std::vector <double> temp;
+    for(int j = 0; j < size; j++){
+            temp.push_back(0);
+        }
+    for(int i = 0; i < size; i++){
+        matrix.push_back(temp);
+    }
 }
 
 matrixTask::~matrixTask(){};
 
-void matrixTask::set(int posN, int posM, int value){
-
+void matrixTask::set(int posN, int posM, double value){
+    matrix.at(posN).at(posM) = value;
 }
 
 double matrixTask::get(int posN, int posM){
@@ -49,12 +58,31 @@ int matrixTask::rows(){
     return matrix.size();
 }
 
+void matrixTask::print(){
+    
+    for(int i = 0; i < rows(); i++){
+        for(int j = 0; j < cols(); j++){
+            std::cout << std::setw(4) << matrix[i][j];
+        }
+        std::cout << std::endl;
+    }
+}
+
 int main(){
     matrixTask firstMatrix = matrixTask(3,4);
     int size = firstMatrix.cols();
     std::cout << size;
+
     int maxSize = firstMatrix.rows();
     std::cout << std::endl <<maxSize;
+    std::cout << std::endl;
+    firstMatrix.set(0,0,1);
+    firstMatrix.print();
+    
+    std::cout << std::endl;
+    //matrixTask secondMatrix = matrixTask(4);
+    //secondMatrix.print();
+
 
     return 0;
 }
