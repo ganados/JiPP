56a77e53ad55ea8afe1920a97adcf4a6eb080180
ganados <przemek4121@gmail.com>
2020-11-14 20:26:04 +0100

    Test added, loading error

--- JiPPLab3/item1.cpp
+++ JiPPLab3/item1.cpp
@@ -1,197 +1,56 @@
-#include <iostream>
-#include <vector>
-#include <iomanip>
-#include <string>
-#include <fstream>
-
-class matrixTask {
-    private:
-        std::vector < std::vector < double > > matrix; 
-    public:
-        matrixTask(int rows, int cols);
-        matrixTask(int size);
-        ~matrixTask();
-        void set(int posN, int posM, double value);
-        double get(int posN, int posM);
-        matrixTask add(matrixTask m2);
-        matrixTask subtract(matrixTask m2);
-        matrixTask multiply(matrixTask m2);
-        int cols();
-        int rows();
-        void print();
-        bool store(std::string fileName, std::string filePath);
-        matrixTask(std::string filePath);
-};
-
-matrixTask::matrixTask(int rows, int cols){
-    std::vector <double> temp;
-    for(int j = 0; j < cols; j++){
-            temp.push_back(0);
-        }
-    for(int i = 0; i < rows; i++){
-        matrix.push_back(temp);
-    }
-}
-matrixTask::matrixTask(int size){
-    std::vector <double> temp;
-    for(int j = 0; j < size; j++){
-            temp.push_back(0);
-        }
-    for(int i = 0; i < size; i++){
-        matrix.push_back(temp);
-    }
-}
-matrixTask::matrixTask(std::string filePath){
-    std::ifstream file;
-    file.open(filePath.c_str());
-    if (file.good()){
-        int rows, cols;
-        file >> rows >> cols;
-        int trash;
-        std::vector <double> temp;
-        for(int j = 0; j < cols; j++){
-                file >> trash;
-                temp.push_back(trash);
-            }
-        for(int i = 0; i < rows; i++){
-            matrix.push_back(temp);
-      }
-    }
-    file.close();
-}
-
-matrixTask::~matrixTask(){};
-
-matrixTask matrixTask::add(matrixTask m2){
-    int nRows = rows();
-    int nCols = cols();
-    matrixTask resultMatrix = matrixTask(nRows, nCols);
-    for(int i = 0; i < nRows; i++){
-        for(int j = 0; j < nCols; j++){
-            (resultMatrix.matrix)[i][j] = matrix[i][j] + (m2.matrix)[i][j];
-        }
-    }
-    return resultMatrix;    
-}
-
-matrixTask matrixTask::subtract(matrixTask m2){
-    int nRows = rows();
-    int nCols = cols();
-    matrixTask resultMatrix = matrixTask(nRows, nCols);
-    for(int i = 0; i < nRows; i++){
-        for(int j = 0; j < nCols; j++){
-            (resultMatrix.matrix)[i][j] = matrix[i][j] - (m2.matrix)[i][j];
-        }
-    }
-    return resultMatrix;    
-}
-
-matrixTask matrixTask::multiply(matrixTask m2){
-    int nRows = rows();
-    int nCols = cols();
-    int m2Rows = m2.rows();
-    int m2Cols = m2.cols();
-    int valueHolder = 0;
-    matrixTask resultMatrix = matrixTask(m2Cols, nRows);
-    for(int i = 0; i < nRows; i++){
-        for(int j = 0; j < m2Cols; j++){
-            valueHolder = 0;
-            for(int k = 0; k < m2Rows; k++){
-                valueHolder += matrix[i][k] * (m2.matrix)[k][j];
-            }
-            (resultMatrix.matrix)[i][j] = valueHolder;
-        }     
-    }
-    return resultMatrix;  
-}
-
-void matrixTask::set(int posN, int posM, double value){
-    matrix.at(posN).at(posM) = value;
-}
-
-double matrixTask::get(int posN, int posM){
-    return matrix.at(posN).at(posM);
-}
-
-int matrixTask::cols(){
-    return matrix[0].size();
-}
-
-int matrixTask::rows(){
-    return matrix.size();
-}
-
-void matrixTask::print(){
-    
-    for(int i = 0; i < rows(); i++){
-        for(int j = 0; j < cols(); j++){
-            std::cout << std::setw(4) << matrix[i][j];
-        }
-        std::cout << std::endl;
-    }
-}
-
-bool matrixTask::store(std::string fileName, std::string filePath)
-{
-    std::ofstream file;
-    file.open(fileName.c_str());
-    if (!file.good())
-        return false;
-
-    int nRows = rows();
-    int nCols = cols();
-
-    file << nRows << " " << nCols;
-    file << std::endl;
-    for(int i = 0; i < nRows; i++){
-        for(int j = 0; j < nCols; j++){
-            file << matrix[i][j] << " ";
-        }
-        file << std::endl;
-    }
-/*
-    std::string wiersz;
-    while (std::getline(file, wiersz)) {
-    }
-*/
-    file.close();
-    return true;
-}
+#include "./Headers/matrices.hpp"
 
 int main(){
-    matrixTask firstMatrix = matrixTask(2,2);
-    matrixTask secondMatrix = matrixTask(2,2);
-    firstMatrix.set(0,0,1);
-    firstMatrix.set(0,1,1);
-    firstMatrix.set(1,0,1);
-    firstMatrix.set(1,1,1);
+    matrixTask firstMatrix = matrixTask(5,6);
+    matrixTask secondMatrix = matrixTask(6,5);
+    matrixTask rectMatrix = matrixTask(5);
+    matrixTask rectMatrix2 = matrixTask(5);
+    srand(time(NULL));
+    double temp;
+// uzupelnienie macierzy
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 6; j++){
+            temp = (double)(rand() % 1000 + 1) / (i * j * 10 + 1);
+            firstMatrix.set(i,j,temp);
+        }
+    }
+    for(int i = 0; i < 6; i++){
+        for(int j = 0; j < 5; j++){
+            temp = (double)(rand() % 100 + 1) / (i * j + 1);
+            secondMatrix.set(i,j,temp);
+        }
+    }
+    for(int i = 0; i < 5; i++){
+        for(int j = 0; j < 5; j++){
+            temp = (double)(rand() % 1000 + 1) / (i * j * 10 + 1);
+            rectMatrix.set(i,j,temp);
+            temp = (double)(rand() % 1000 + 1) / (i * j * 10 + 1);
+            rectMatrix2.set(i,j,temp);
+        }
+    }
+    std::cout << "---------------------------- firstMatrix 5x6 ----------------------------" <<std::endl;
     firstMatrix.print();
-
-    std::cout << std::endl;
-
-    secondMatrix.set(0,0,2);
-    secondMatrix.set(0,1,2);
-    secondMatrix.set(1,0,2);
-    secondMatrix.set(1,1,2);
+    std::cout << "---------------------------- secondMatrix 6x5 ----------------------------" <<std::endl;
     secondMatrix.print();
-
-    std::cout << std::endl;
-
-    matrixTask resultMatrix = firstMatrix.add(secondMatrix);
+    std::cout << "---------------------------- rectMatrix 5x5 ----------------------------" <<std::endl;
+    rectMatrix.print();
+    std::cout << "---------------------------- rectMatrix2 5x5 ----------------------------" <<std::endl;
+    rectMatrix2.print();
+    std::cout << "---------------------------- ADD ----------------------------" <<std::endl;
+    matrixTask resultMatrix = rectMatrix.add(rectMatrix2);
     resultMatrix.print();
-
-    std::cout << std::endl;
-
-    matrixTask resultMatrix2 = firstMatrix.subtract(secondMatrix);
+    std::cout << "---------------------------- SUBTRACT ----------------------------" <<std::endl;
+    matrixTask resultMatrix2 = rectMatrix.subtract(rectMatrix2);
     resultMatrix2.print();
 
-    std::cout << std::endl;
-
+    std::cout << "---------------------------- MULTIPLY  ----------------------------" <<std::endl;
     matrixTask resultMatrix3 = firstMatrix.multiply(secondMatrix);
     resultMatrix3.print();
     std::cout << std::endl;
-    //resultMatrix3.store("pliczek", "./");
+    std::cout << "---------------------------- Store matrix in file  ----------------------------" <<std::endl;
+    resultMatrix3.store("pliczekStore", "./");
 
+    std::cout << "---------------------------- constructor from file ----------------------------" <<std::endl;
     matrixTask resultMatrix4 = matrixTask("./pliczek");
     resultMatrix4.print();
 
