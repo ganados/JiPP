7e885ce7f705948ddf842560de4992631200d724
ganados <przemek4121@gmail.com>
2020-11-24 16:19:50 +0100

    Added cmake, run make file

---
+++ JiPPLab4/make.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ ! -d './build' ]; then
+    echo `mkdir build`
+fi
+
+if [ -f "./CMakeLists.txt" ]; then
+    echo `cmake -B build -G "MinGW Makefiles"`
+    echo `mingw32-make --directory=build`
+    exit
+fi
--- JiPPLab4/src/item1.cpp
+++ JiPPLab4/src/item1.cpp
@@ -47,11 +47,11 @@ int main(){
     resultMatrix3.print();
     std::cout << std::endl;
     std::cout << "---------------------------- Store matrix in file  ----------------------------" <<std::endl;
-    if(resultMatrix3.store("pliczekStore", "./txtFiles/"))
+    if(resultMatrix3.store("pliczekStore", "./"))
         std::cout << "The file has been saved" << std::endl;
 
     std::cout << "---------------------------- constructor from file ----------------------------" <<std::endl;
-    matrixTask resultMatrix4 = matrixTask("./txtFiles/pliczek");
+    matrixTask resultMatrix4 = matrixTask("./pliczek");
     resultMatrix4.print();
 
     return 0;
